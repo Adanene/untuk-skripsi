7484b38814fa0602430d8ea52be3037b34eb8f6d
Adanene <128061161+Adanene@users.noreply.github.com>
2023-03-17 09:26:36 +0700

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -12,79 +12,18 @@ def shorten_categories(categories, cutoff):
     return categorical_map
 
 
-def clean_experience(x):
-    if x ==  'More than 50 years':
-        return 50
-    if x == 'Less than 1 year':
-        return 0.5
-    return float(x)
-
-
-def clean_education(x):
-    if 'Bachelor’s degree' in x:
-        return 'Bachelor’s degree'
-    if 'Master’s degree' in x:
-        return 'Master’s degree'
-    if 'Professional degree' in x or 'Other doctoral' in x:
-        return 'Post grad'
-    return 'Less than a Bachelors'
-
-
 @st.cache
 def load_data():
-    df = pd.read_csv("survey_results_public.csv")
-    df = df[["Country", "EdLevel", "YearsCodePro", "Employment", "ConvertedCompYearly"]]
-    df = df[df["ConvertedCompYearly"].notnull()]
-    df = df.dropna()
-    df = df[df["Employment"] == "Employed full-time"]
-    df = df.drop("Employment", axis=1)
-
-    country_map = shorten_categories(df.Country.value_counts(), 400)
-    df["Country"] = df["Country"].map(country_map)
-    df = df[df["ConvertedCompYearly"] <= 250000]
-    df = df[df["ConvertedCompYearly"] >= 10000]
-    df = df[df["Country"] != "Other"]
-
-    df["YearsCodePro"] = df["YearsCodePro"].apply(clean_experience)
-    df["EdLevel"] = df["EdLevel"].apply(clean_education)
-    df = df.rename({"ConvertedCompYearly": "Salary"}, axis=1)
+    df = pd.read_excel(f"https://docs.google.com/spreadsheets/d/1vSeqmo559uXl_sjwCorKsENK2boOihERFrmrM-V5D2Y/export?format-xlsx" , 'Sheet1' , header = 0)
     return df
 
 df = load_data()
 
 def show_explore_page():
-    st.title("Explore Software Engineer Salaries")
+    st.title("Explore Ship Stability")
 
     st.write(
         """
-    ### Stack Overflow Developer Survey 2020
+    ### Inclining test adalah percobaan kemiringan yang harus dilakukan untuk mengetahui berat dan letak titik berat kapal kosong setelah selesai dibangun (Biro Klasifikasi Indonesia,2003). Pengujian ini dilakukan untuk memenuhi persyaratan class dalam rangkan pemenuhan persyaratan statutory untuk Badan Pemerintah.
     """
     )
-
-    data = df["Country"].value_counts()
-
-    fig1, ax1 = plt.subplots()
-    ax1.pie(data, labels=data.index, autopct="%1.1f%%", shadow=True, startangle=90)
-    ax1.axis("equal")  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-    st.write("""#### Number of Data from different countries""")
-
-    st.pyplot(fig1)
-    
-    st.write(
-        """
-    #### Mean Salary Based On Country
-    """
-    )
-
-    data = df.groupby(["Country"])["Salary"].mean().sort_values(ascending=True)
-    st.bar_chart(data)
-
-    st.write(
-        """
-    #### Mean Salary Based On Experience
-    """
-    )
-
-    data = df.groupby(["YearsCodePro"])["Salary"].mean().sort_values(ascending=True)
-    st.line_chart(data)
