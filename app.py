ac0ede3aa6588571fd50686555cd749cf95f97a9
Adanene <128061161+Adanene@users.noreply.github.com>
2023-03-17 10:44:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 import streamlit as st
-predict_page = st.file_uploader(predict_page.py)
-explore_page = st.file_uploader(explore_page.py)
-    
-    
-from predict_page import show_predict_page
-from explore_page import show_explore_page
+ 
+from predict_page.py import show_predict_page
+from explore_page.py import show_explore_page
     
 
     
